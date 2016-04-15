13b96f5ffbc2c098ee76b1549f8973cbe69b8736
Anssi Kostiainen <anssi.kostiainen@gmail.com>
2016-04-15 11:17:25 +0300

    make-valid.sh no longer needed after changes in commit 6185e6959cb83612c8feca1e83d9e367748cd426

--- releases/make-valid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-if [ "$#" -ne 2 ]; then
-  echo "Make MathJax output valid HTML"
-  echo "Usage: ./make-valid.sh invalid.html valid.html"
-else
-  sed 's/<nobr/<span class="nobr"/;s/<\/nobr>/<\/span>/;s/.MathJax nobr/.MathJax .nobr/' $1 > $2
-fi
